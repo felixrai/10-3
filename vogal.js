34888a1f63c9549bb7b6ea93ebdeab85567fa8b2
felixrai <133838065+felixrai@users.noreply.github.com>
2023-10-21 18:59:45 -0300

    Update vogal.js

--- vogal.js
+++ vogal.js
@@ -4,7 +4,7 @@ var cont = 0;
 texto = texto.toLowerCase();
 
 for(var i = 0; i < texto.length; i ++) {
-    if (texto.charAt(i)=="a" ||  texto.charAt(i)=="e" ||  texto.charAt(i)=="i" ||  texto.charAt(i)=="o" ||  texto.charAt(i)=="u" ) {
+    if (texto.charAt(i)=="a" ||  texto.charAt(i)=="e" ||  texto.charAt(i)=="i" ||  texto.charAt(i)=="o" ||  texto.charAt(i)=="u"  ||  texto.charAt(i)=="á" ||  texto.charAt(i)=="é"  ||  texto.charAt(i)=="í"  ||  texto.charAt(i)=="ó"  ||  texto.charAt(i)=="ú"  ||  texto.charAt(i)=="â" ||  texto.charAt(i)=="ê"  ||  texto.charAt(i)=="ô" ) {
         cont++;
     }
 }
